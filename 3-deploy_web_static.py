9de0de543df77ba1d5e50ee6837c10c48b74509a
Sofiag8 <1943@holbertonschool.com>
2020-12-23 20:06:46 +0000

    pep

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -11,6 +11,7 @@ from os import path
 env.hosts = ['35.196.158.6', '35.227.106.6']
 env.password = 'betty'
 
+
 def do_pack():
     """
     All files in the folder web_static must be added to
@@ -44,18 +45,30 @@ def do_deploy(archive_path):
     if not path.exists(archive_path):
         return False
     try:
+        #  var to get the file name only separate by / and get last pos
         file_name = archive_path.split("/")[-1]
-        folder_to_compress = ("/data/web_static/releases/"
-                              + file_name.split(".")[0])
+        # set var for path to uncomp and set only the compressed
+        # file without the extension, divided by "." and first[0] pos.
+        folder_to_compress = ("/data/web_static/releases/" +
+                              file_name.split(".")[0])
+        #  uploading the archive_path to tmp on the server"""
         put(archive_path, "/tmp/")
+        #  run is used to execute a shell command on remote servers
         run("mkdir -p {}".format(folder_to_compress))
-        run("tar -xvzf /tmp/{} -C {}".format(file_name,
-                                                 folder_to_compress))
+        #  uncompress archive to specific folder using the "-C" option
+        #  tar -xvzf abc.tar.gz -C /opt/folder/
+        #  tar compresses multiple files
+        run("tar -xzf /tmp/{} -C {}".format(file_name,
+                                            folder_to_compress))
+        #  Delete the archive from the web server
         run("rm /tmp/{}".format(file_name))
         run("mv {}/web_static/* {}/".format(folder_to_compress,
-                                                 folder_to_compress))
+                                            folder_to_compress))
         run("rm -rf {}/web_static".format(folder_to_compress))
+        #  delete symbolic link
         run("rm -rf /data/web_static/current")
+        #  create a new symbolic link  ln -s /usr/local/var /var/run
+        #  on the server, create a linked with the current
         run("ln -s {} /data/web_static/current".format(folder_to_compress))
         return True
     except:
