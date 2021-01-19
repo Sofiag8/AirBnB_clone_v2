b3f0d142eed7edafc78a4885d89903eaa95b1aff
Sofiag8 <1943@holbertonschool.com>
2021-01-19 18:11:57 +0000

    python is cool

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" script that starts a Flask web application
+on port 5000 and displays Hello HBNB"""
+
+from flask import Flask, escape
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def Hello():
+    """ displaying a default Hello """
+    return 'hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def HBNB():
+    """ display HBNB """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def C(text):
+    """ display C with the passed keyword argument var text """
+    return 'C %s' % text.replace(text)
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text="is cool"):
+    """ display python with the passed keyword argument var text """
+    return 'Python %s' % text.replace("_", " ")
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="5000")
